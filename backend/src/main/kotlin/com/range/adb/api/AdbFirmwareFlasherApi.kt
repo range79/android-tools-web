7064261c182c7d2dfc417f933b461e19123c203c
range79 <darkrange006@gmail.com>
2025-08-28 16:04:18 +0400

    feat: fastboot flashing controller and api implemented

--- backend/src/main/kotlin/com/range/adb/api/AdbFirmwareFlasherApi.kt
+++ backend/src/main/kotlin/com/range/adb/api/AdbFirmwareFlasherApi.kt
@@ -8,6 +8,6 @@ import java.util.UUID
 
 @RequestMapping("/adb")
 interface AdbFirmwareFlasherApi {
-    @PostMapping("/flash/{deviceId}/flash/{firmwareId}")
+    @PostMapping("/flash/{deviceId}/{firmwareId}")
     fun install(@PathVariable firmwareId: UUID, @PathVariable deviceId: Long): AdbDeviceResponseDto
 }
