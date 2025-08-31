5482e2b599cf477e20586b615f39cd81d223c39d
range79 <157882017+range79@users.noreply.github.com>
2025-08-31 22:03:48 +0400

    Revert "fix: url endpoints"

--- frontend/src/components/firmware/FirmwareList.jsx
+++ frontend/src/components/firmware/FirmwareList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { deleteFirmware, getAllFirmwares } from '../../api/firmware';
 import Sidebar from '../Sidebar';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { flashFirmware, getDeviceList } from '../../api/adb';
 import { flashFastbootDevice, getFastbootDeviceList, getPartitionOptions } from '../../api/fastboot';
 import { PlusIcon, Trash2Icon } from "lucide-react";
@@ -252,7 +252,7 @@ const FirmwareList = () => {
             </table>
           </div>
         )}
-        <Link to={"/firmware/upload"}><button className='absolute top-10 right-10 bg-green-600 p-3 rounded-lg hover:bg-green-700 transition duration-200 text-center items-center justify-center flex gap-1 hover:cursor-pointer'><PlusIcon/> Upload Firmware</button></Link>
+        <a href="http://localhost:5173/firmware/upload"><button className='absolute top-10 right-10 bg-green-600 p-3 rounded-lg hover:bg-green-700 transition duration-200 text-center items-center justify-center flex gap-1 hover:cursor-pointer'><PlusIcon/> Upload Firmware</button></a>
       </div>
     </div>
   );
